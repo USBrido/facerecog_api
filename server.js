25373b18182e3cacce9c08acac85b3c05b50b826
Rodrigo <obrido@gmail.com>
2020-06-16 17:12:56 -0400

    code clean up, setting up controllers folder

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
 const knex = require("knex");
 const pg = require("pg");
+const signin = require("./controllers/signin");
+const register = require("./controllers/register");
 
 const app = express();
 
@@ -24,57 +26,9 @@ app.get("/", (req, res) => {
   res.send(database.users);
 });
 
-app.post("/signin", (req, res) => {
+app.post("/signin", (req, res) => signin.handleSignin(req, res, db, bcrypt));
 
-  db.select('email', 'hash')
-    .from('login')
-    .where('email', '=', req.body.email)
-    .then(data=> {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      if (isValid) {
-        return db.select('*')
-          .from('users')
-          .where('email', '=', req.body.email)
-          .then(user => {
-            console.log(user);
-            res.json(user[0]);
-          })
-          .catch(error =>res.status(400).json('unable to retrieve user'));
-      } else {
-        res.status(400).json('Name and Password combination are not correct');
-      }
-    })
-    .catch(error =>res.status(400).json('unable to login-in'));
-});
-
-app.post("/register", (req, res) => {
-  const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password);
-  db.transaction(trx => {
-    trx
-      .insert({
-        hash: hash,
-        email: email
-      })
-      .into("login")
-      .returning("email")
-      .then(loginEmail => {
-        return trx("users")
-          .returning("*")
-          .insert({
-            email: loginEmail[0],
-            name: name,
-            joined: new Date()
-          })
-          .then(user => {
-            res.json(user[0]);
-          });
-      })
-      .then(trx.commit)
-      .catch(trx.rollback);
-  })
-    .catch(error => res.status(400).json("Unable to register"));
-});
+app.post("/register", (req, res) => register.handleRegister(req, res, db, bcrypt));
 
 app.get("/profile/:id", (req, res) => {
   const { id } = req.params;
